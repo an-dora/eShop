94e76a8bac71de039cdc6199d41e02c7af84f207
An Dora <cstiwap@gmail.com>
2022-11-09 13:15:28 +0700

    up1

---
+++ DoAn2VADT/Database/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using DoAn2VADT.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoAn2VADT.Database
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Distributor> Distributors { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Title> Titles { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ DoAn2VADT/ViewModels/Book/ListItemBook.cs
@@ -0,0 +1,16 @@
+﻿namespace DoAn2VADT.ViewModels.Book
+{
+    public class ListItemBook
+    {
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public decimal? Price { get; set; }
+        public decimal? Discount { get; set; }
+
+        public string? DistributorName { get; set; }
+        public string? PublisherName { get; set; }
+        public string? AuthorName { get; set; }
+        public string? TitleName { get; set; }
+        public string? CategoryName { get; set; }
+    }
+}
